282ac6beb28c0d9440e006e681fbb84d660e5122
Ármin Scipiades <scipiades.armin@gmail.com>
2021-07-21 08:44:11 +0200

    Add footer, spruce up info page, privacy page

--- src/InfoPage.vue
+++ src/InfoPage.vue
@@ -4,51 +4,64 @@
       <p class="title">
         Mi ez, mire jó?
       </p>
-      <p>
-        A <a href='https://molbubi.hu/'>BuBi a budapesti közbringarendszer</a>, 2021-es megújulása óta
-        jó alternatívája a tömegközlekedésnek, autózásnak a tágan vett belvárosban.
-      </p>
+      <div class="content">
+        <p>
+          A <a href='https://molbubi.hu/'>Bubi a budapesti közbringarendszer</a>, 2021-es megújulása óta
+          jó alternatívája a tömegközlekedésnek, autózásnak a tágan vett belvárosban.
+        </p>
 
-      <p>
-        Ebben az appban nyilvántarthatod a bubis útjaidat, hogy érdekes statisztikákat ismerhess meg a bubizásodról.
-        Vajon melyik bringát használtad a legtöbbet?
-        Melyik állomást érintetted a legtöbbször?
-        Hány bringával mentél összesen?
-      </p>
+        <p>
+          Ebben az appban nyilvántarthatod a bubis útjaidat, hogy érdekes statisztikákat ismerhess meg a bubizásodról.
+          <ul>
+            <li>Vajon melyik bringát használtad a legtöbbet?</li>
+            <li>Melyik állomást érintetted a legtöbbször?</li>
+            <li>Hány bringával mentél összesen?</li>
+            <li>Hány egymást követő napon használtad a Bubit?</li>
+          </ul>
+        </p>
+      </div>
     </div>
 
     <div class="hero-body">
       <p class="title">
         Hogy működik?
       </p>
-      <p>
-        A BuBi appból nem lehet automatikusan kinyerni a felhasználói előzményeket, ezért neked kell az útjaid adatait kézzel bevinni - de nyugi, ez egész fájdalommentes.
-      </p>
+      <div class="content">
+        <p>
+          A BuBi appból nem lehet automatikusan kinyerni a felhasználói előzményeket, ezért neked kell az útjaid adatait kézzel bevinni - de nyugi, ez egész fájdalommentes.
+        </p>
+      </div>
     </div>
 
     <div class="hero-body">
       <p class="title">
         Ennyi az egész?
       </p>
-      <p>
-        Egyelőre igen.
-        De tervezzük, hogy megcsináljuk a Strava integrációt, ami lehetővé teszi majd, hogy a Stravára feltett bubizásaidat automatikusan megjelöljük, ami elég menő.
-      </p>
+      <div class="content">
+        <p>
+          Sok tervünk van, igyekszünk gyorsan fejleszteni az applikációt.
+        </p>
+        <p>
+          A távlati terv, hogy megcsináljuk a Strava integrációt, ami lehetővé teszi majd, hogy a Stravára feltett bubizásaidat automatikusan megjelöljük, ami elég menő.
+        </p>
+      </div>
     </div>
 
     <div class="hero-body">
       <p class="title">
         Ingyen van? Biztonságos? Ki csinálja?
       </p>
-        Ingyen van és biztonságos.
-        Olvasd el az <router-link :to="{ path: '/privacy' }">adatkezelési tájékoztatónkat</router-link>.
-        A lényeg az, hogy az adataidat titkosítva tároljuk, harmadik félnek nem adjuk át.
-      <p>
-      </p>
-      <p>
-        A BubiStatot <a href="mailto:scipiades.armin@gmail.com">Scipiades Ármin</a> készítette, mert akart egy ilyent.
-        Ha kérdésed vagy kívánságod van, nyugodtan írj neki, örülni fog.
-      </p>
+      <div class="content">
+        <p>
+          Ingyen van és biztonságos.
+          Olvasd el az <router-link :to="{ path: '/privacy' }">adatkezelési tájékoztatónkat</router-link>.
+          A lényeg az, hogy az adataidat titkosítva tároljuk, harmadik félnek nem adjuk át.
+        </p>
+        <p>
+          A BubiStatot <a href="mailto:scipiades.armin@gmail.com">Scipiades Ármin</a> készítette, mert akart egy ilyent.
+          Ha kérdésed vagy kívánságod van, nyugodtan írj neki, örülni fog.
+        </p>
+      </div>
     </div>
   </section>
 </template>
--- src/Privacy.vue
+++ src/Privacy.vue
@@ -3,23 +3,25 @@
     <div class="hero-body">
       <p class="title">Adatkezelési tájékoztató</p>
 
-      <p>
-        Az általad megadott adatokat az emailcímedhez kapcsolva, titkosítva tároljuk, a böngésződbe titkosított csatornán küldjük.
-        Használjuk a Google Analytics szolgáltatást, ami anonimizált adatokat szolgáltat az oldal látogatottságáról.
-        Más adatot nem gyűjtünk rólad.
-      </p>
-      <p>
-        Semmilyen adatot nem adunk át rólad harmadik félnek.
-      </p>
-      <p>
-        Az alkalmazás cookie-kat használ.
-      </p>
-      <p>
-        Az alkalmazás forráskódja megtekinthető a GitHubon.
-      </p>
-      <p>
+      <div class="content">
+        <p>
+          Az általad megadott adatokat az emailcímedhez kapcsolva, titkosítva tároljuk, a böngésződbe titkosított csatornán küldjük.
+          Használjuk a Google Analytics szolgáltatást, ami anonimizált adatokat szolgáltat az oldal látogatottságáról.
+          Más adatot nem gyűjtünk rólad.
+        </p>
+        <p>
+          Semmilyen adatot nem adunk át rólad harmadik félnek.
+        </p>
+        <p>
+          Az alkalmazás cookie-kat használ.
+        </p>
+        <p>
+          Az alkalmazás forráskódja <a href='https://github.com/Botffy/BubiStats' target="_blank">elérhető a GitHubon</a>.
+        </p>
+        <p>
 
-      </p>
+        </p>
+      </div>
     </div>
   </section>
 </template>
